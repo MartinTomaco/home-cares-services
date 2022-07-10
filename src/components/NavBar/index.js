818a51c100773587041eac1bb1ec63ab2e37649b
Martin Gonzalez <marttingonzalez@gmail.com>
2022-07-10 15:01:25 -0300

    Create language button and style

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import logo from '../../assets/logo_hcs.png'
-import { MdSearch, MdMenu } from 'react-icons/md';
+import { MdMenu } from 'react-icons/md';
 import '../NavBar/styles.css';
 
 const SIZE = '32px';
@@ -9,15 +9,21 @@ export const NavBar = () => {
   return (
     <header>
       <nav>
-        <a href='/'>
+        <section className='navLeft'>
+
           <img className='imgLogo' alt={'logo'} src={logo}></img>
-        </a>
-        <a href='/'>
-          <MdSearch size={SIZE} />
-        </a>
-        <a href='/'>
-          <MdMenu size={SIZE} />
-        </a>
+
+        </section>
+
+
+        <section className='navRight'>
+
+            <button>ES</button>
+            <button>EN</button>
+
+            <MdMenu size={SIZE} />
+
+        </section>
       </nav>
     </header>
   );
