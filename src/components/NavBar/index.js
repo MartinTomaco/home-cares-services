8ad41a6c9ce4d7e55e44cf288114c3fc8fd41a35
mar77in <marttingonzalez@gmail.com>
2022-10-24 12:59:47 -0300

    feature: add IT and BR language

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react';
-import { HamburMenu } from '../HamburMenu';
+// import { HamburMenu } from '../HamburMenu';
 import LangContext from '../../context/LangContext';
 import logo from '../../assets/logo_mec.png'
 import '../NavBar/styles.css';
@@ -10,6 +10,8 @@ export const NavBar = () => {
      console.log('langFromContext:',lang)
     if (event.target.innerText === 'ES') { setLang('ES') }
     if (event.target.innerText === 'EN') { setLang('EN') }
+    if (event.target.innerText === 'IT') { setLang('IT') }
+    if (event.target.innerText === 'BR') { setLang('BR') }
   }
   return (
     <header>
@@ -24,7 +26,9 @@ export const NavBar = () => {
         <section className='navRight'>
 
           <button onClick={(event) => handleClick(event)}>ES</button>
+          <button onClick={(event) => handleClick(event)}>IT</button>
           <button onClick={(event) => handleClick(event)}>EN</button>
+          <button onClick={(event) => handleClick(event)}>BR</button>
           {/* <HamburMenu/> To be implemented...*/} 
           
 
